8862449c8677696e636bb35f1c8d256c04ab68f5
aleesa21 <kaflealeesa@gmail.com>
2026-05-28 13:36:09 +0545

    about Layout added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,72 +1,16 @@
-export default function About(){
-    return (
-       <section id="about">
-        about about about about about about about about about about about about
-        about about about about about about about aboutabout about about about
-        about about about about about aboutabout about about about about about
-        about about about aboutabout about about about about about about about
-        about aboutabout about about about about about about about about
-        aboutabout about about about about about about about about aboutabout
-        about about about about about about about about aboutabout about about
-        about about about about about about aboutabout about about about about
-        about about about about aboutabout about about about about about about
-        about about aboutabout about about about about about about about about
-        aboutabout about about about about about about about about aboutabout
-        about about about about about about about about aboutabout about about
-        about about about about about about aboutabout about about about about
-        about about about about aboutabout about about about about about about
-        about about aboutabout about about about about about about about about
-        aboutabout about about about about about about about about aboutabout
-        about about about about about about about about aboutabout about about
-        about about about about about about aboutabout about about about about
-        about about about about aboutabout about about about about about about
-        about about aboutabout about about about about about about about about
-        aboutabout about about about about about about about about aboutabout
-        about about about about about about about about aboutabout about about
-        about about about about about about aboutabout about about about about
-        about about about about aboutabout about about about about about about
-        about about aboutabout about about about about about about about about
-        aboutabout about about about about about about about about aboutabout
-        about about about about about about about about aboutabout about about
-        about about about about about about aboutabout about about about about
-        about about about about aboutabout about about about about about about
-        about about aboutabout about about about about about about about about
-        aboutabout about about about about about about about about aboutabout
-        about about about about about about about about aboutabout about about
-        about about about about about about aboutabout about about about about
-        about about about about aboutabout about about about about about about
-        about about aboutabout about about about about about about about about
-        aboutabout about about about about about about about about aboutabout
-        about about about about about about about about aboutabout about about
-        about about about about about about aboutabout about about about about
-        about about about about aboutabout about about about about about about
-        about about aboutabout about about about about about about about about
-        aboutabout about about about about about about about about aboutabout
-        about about about about about about about about aboutabout about about
-        about about about about about about aboutabout about about about about
-        about about about about aboutabout about about about about about about
-        about about aboutabout about about about about about about about about
-        aboutabout about about about about about about about about aboutabout
-        about about about about about about about about aboutabout about about
-        about about about about about about aboutabout about about about about
-        about about about about aboutabout about about about about about about
-        about about aboutabout about about about about about about about about
-        aboutabout about about about about about about about about aboutabout
-        about about about about about about about about aboutabout about about
-        about about about about about about aboutabout about about about about
-        about about about about aboutabout about about about about about about
-        about about aboutabout about about about about about about about about
-        aboutabout about about about about about about about about aboutabout
-        about about about about about about about about aboutabout about about
-        about about about about about about aboutabout about about about about
-        about about about about aboutabout about about about about about about
-        about about aboutabout about about about about about about about about
-        aboutabout about about about about about about about about aboutabout
-        about about about about about about about about aboutabout about about
-        about about about about about about aboutabout about about about about
-        about about about about aboutabout about about about about about about
-        about about aboutabout about about about about about about about about
-        aboutabout about about about about about about about about aboutv
-      </section>
-    )
+import "../css/about.css";
+export default function About() {
+  return (
+    <section id="about">
+      <div className="about_blob"></div>
+      <div className="about_main">
+        <div className="about_left">
+          <div className="about_img_container">
+            <img className="about_image" src="" aly=""></img>
+          </div>
+        </div>
+        <div className="about_right">right</div>
+      </div>
+    </section>
+  );
 }
